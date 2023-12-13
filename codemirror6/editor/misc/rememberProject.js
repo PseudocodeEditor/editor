f66d7889791b2038cbc60f2f79f3fc98e87616cf
Coding-Cactus <codingcactus.cc@gmail.com>
2023-12-13 16:36:25 +0000

    :children_crossing: Remove unnecessary dialogues when remembering projects

--- codemirror6/editor/misc/rememberProject.js
+++ codemirror6/editor/misc/rememberProject.js
@@ -3,13 +3,10 @@ import {bundleProject} from "../project/download.js";
 
 setTimeout(() => {
   if (localStorage.getItem("project") !== null) {
-    if (confirm("A recent session has been detected, would you like to restore it?")) {
       openProject(localStorage.getItem("project"));
-    }
   }
 }, 100);
 
 window.onbeforeunload = () => {
   localStorage.setItem("project", bundleProject());
-  return "File changes won't be saved unless downloaded.";
 };
