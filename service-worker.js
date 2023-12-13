6f767c486b5ab33a151f3a0ae66a17cc4d26baf4
Coding-Cactus <codingcactus.cc@gmail.com>
2023-12-13 11:57:45 +0000

    :sparkles: Add service worker

---
+++ service-worker.js
@@ -0,0 +1,61 @@
+const cacheName = "cache-v1";
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll([
+                "/",
+                "/index.html",
+
+                "/dist/editor.bundle.js",
+
+                "/css/style.css",
+                "/css/editor.css",
+
+                "/media/App-Icon.png",
+                "/media/favicon.ico",
+                "/media/file.svg",
+                "/media/psc.svg",
+
+                "/PS2/main.py",
+                "/PS2/ps2/__init__.py",
+                "/PS2/ps2/app.py",
+                "/PS2/ps2/expr/__init__.py",
+                "/PS2/ps2/expr/expression.py",
+                "/PS2/ps2/interpret/__init__.py",
+                "/PS2/ps2/interpret/interpretor.py",
+                "/PS2/ps2/parser/__init__.py",
+                "/PS2/ps2/parser/parser.py",
+                "/PS2/ps2/scan/__init__.py",
+                "/PS2/ps2/scan/ps2_token.py",
+                "/PS2/ps2/scan/scanner.py",
+                "/PS2/ps2/statement/__init__.py",
+                "/PS2/ps2/statement/statement.py",
+                "/PS2/ps2/symbol_table/__init__.py",
+                "/PS2/ps2/symbol_table/environment.py",
+                "/PS2/ps2/utilities.py"
+            ])
+                .then(() => {console.log("Cached files!")})
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+
+        console.log('Fetching', e.request.url);
+
+        if (r && !navigator.onLine) return r;
+
+        const response = await fetch(e.request);
+
+        if (e.request.url.startsWith('http')) {
+            const cache = await caches.open(cacheName);
+            await cache.put(e.request.url, response.clone());
+            console.log(await caches.match(e.request.url))
+        }
+
+        return response;
+    })());
+});
